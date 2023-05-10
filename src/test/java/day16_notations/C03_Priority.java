19c90afa60e9f43e4bc0740b732235300b92845d
muzafferugur <av.muzafferugur@gmail.com>
2023-05-10 14:55:18 +0300

    10.05.2023

--- src/test/java/day16_notations/C03_Priority.java
+++ src/test/java/day16_notations/C03_Priority.java
@@ -9,15 +9,17 @@ public class C03_Priority extends TestBase {
     çalışmasını isterseniz o zaman test methodlarına öncelik (priority) tanımlayabiliriz.
 
     Priority küçükten büyüğe doğru çalışır. Eğer bir teset methoduna priority değeri atanmamışsa
-    default olarak priorty=0 kabul deilir.
+    default olarak priorty=0 kabul edilir.
+
+    priority ne kadar küçükse önce o çalışır. aynıysa isim sıralaması
      */
 
-    @Test(priority = 5)
+    @Test(priority = -5)
     public void amazonTesti() {
         driver.get("https://www.amazon.com");
     }
 
-    @Test
+    @Test(priority = -2)
     public void test02() {
         driver.get("https://www.bestbuy.com");
     }
