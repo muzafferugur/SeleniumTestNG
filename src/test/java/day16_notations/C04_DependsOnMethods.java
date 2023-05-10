7556088fa0f0a62ad54cd042a77b3c1210c8beae
muzafferugur <av.muzafferugur@gmail.com>
2023-05-10 15:34:22 +0300

    10.05.2023

---
+++ src/test/java/day16_notations/C04_DependsOnMethods.java
@@ -0,0 +1,51 @@
+package day16_notations;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class C04_DependsOnMethods {
+    WebDriver driver;
+
+    @BeforeClass
+    public void Setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+        driver.get("https://www.ramazon1.com");
+    }
+
+    @Test(dependsOnMethods ="test01")
+    public void test02() {
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+
+    }
+
+    @Test(dependsOnMethods = "test02")
+    public void test03() {
+        WebElement sonucYazisiElementi = driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"));
+        Assert.assertTrue(sonucYazisiElementi.getText().contains("Nutella"));
+
+    }
+}
