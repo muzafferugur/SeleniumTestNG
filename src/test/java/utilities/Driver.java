38d6f643f4feea88de0fab042ae3b99d767a0f63
muzafferugur <av.muzafferugur@gmail.com>
2023-05-11 16:54:13 +0300

    11.05.2023

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,17 @@
+package utilities;
+
+public class Driver {
+    /*
+    POM'de Driver için TestBase Classına extend etmek yerine  Driver classından
+    static methodlarkullanarak driver oluşturulup,ilgili ayarların yapılması ve en sonda
+    driverın kapatılması tercih edilmiştir.
+     */
+
+    public static void getDriver() {
+
+    }
+
+    public static void closeDriver() {
+
+    }
+}
