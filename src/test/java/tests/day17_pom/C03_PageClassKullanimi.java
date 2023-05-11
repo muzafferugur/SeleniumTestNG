f2d969783d47efb797ab5c19389e3e88a9b5f859
muzafferugur <av.muzafferugur@gmail.com>
2023-05-11 18:37:18 +0300

    11.05.2023

---
+++ src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
@@ -0,0 +1,16 @@
+package tests.day17_pom;
+
+import org.testng.annotations.Test;
+
+public class C03_PageClassKullanimi {
+    @Test
+    public void test01() {
+        /*
+        1.Facebook anasayfaya gidin
+        2.Kullanıcı mail kutusuna rastgele isim yazdırın
+        3.Kullanıcı şifre kutusuna rastgele bir password yazdırın
+        4.Login butonuna basın
+        5.Giriş yapılamadığını test edin
+         */
+    }
+}
