15d2bd603b6afe28375485b939b9d6d9b6bdb8a5
muzafferugur <av.muzafferugur@gmail.com>
2023-05-11 18:50:10 +0300

    11.05.2023

--- src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
+++ src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
@@ -1,6 +1,10 @@
 package tests.day17_pom;
 
+import com.github.javafaker.Faker;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.Driver;
 
 public class C03_PageClassKullanimi {
     @Test
@@ -12,5 +16,21 @@ public class C03_PageClassKullanimi {
         4.Login butonuna basın
         5.Giriş yapılamadığını test edin
          */
+        FacebookPage facebookPage = new FacebookPage();
+
+        Driver.getDriver().get("https://www.facebook.com");
+
+        Faker faker = new Faker();
+        facebookPage.mailKutusu.sendKeys(faker.internet().emailAddress());
+
+        facebookPage.sifreKutusu.sendKeys(faker.internet().password());
+
+        facebookPage.loginTusu.click();
+
+        Assert.assertTrue(facebookPage.girilmediYaziElementi.isDisplayed());
+
+        Driver.closeDriver();
+
+
     }
 }
