23c6fe18acebf46a4b3c345970379af169de809a
muzafferugur <av.muzafferugur@gmail.com>
2023-05-16 17:12:58 +0300

    16.05.2023

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,12 +1,36 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
 
-    Properties properties;
+    public static Properties properties;//->obje oluşturduk
 
-    public String getProperty(String key){
+    static {
+        String dosyaYolu = "configuration.properties";
+
+        //her seferinde class isminin yanına throws atmak yerine try-cathch kullanıyoruz.
+
+        try {
+            FileInputStream fis = new FileInputStream(dosyaYolu);
+            //fis dosya yolunu tanımladığımız configuration.properties dosyasını okudu
+            properties = new Properties();//değer atadık
+            properties.load(fis);// FileInputStream in okuduğu değerleri propertiese yükledim
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+
+        /*
+        test methodundan yolladığımız string key değerini alıp Properties classından getProperty() methodunu
+        kullanarak bu key'e ait value'yu bize getirdi.
+         */
 
         return properties.getProperty(key);
     }
