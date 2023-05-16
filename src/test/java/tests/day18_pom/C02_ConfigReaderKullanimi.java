933dd503fe52cfb0b9e2407533478660359d4a2c
muzafferugur <av.muzafferugur@gmail.com>
2023-05-16 17:29:14 +0300

    16.05.2023

---
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
@@ -0,0 +1,21 @@
+package tests.day18_pom;
+
+import org.testng.annotations.Test;
+
+public class C02_ConfigReaderKullanimi {
+    @Test
+    public void test01() {
+         /*
+        1.Facebook anasayfaya gidin
+        2.Kullanıcı mail kutusuna rastgele isim yazdırın
+        3.Kullanıcı şifre kutusuna rastgele bir password yazdırın
+        4.Login butonuna basın
+        5.Giriş yapılamadığını test edin
+        6.Sayfayı kapatın
+         */
+
+
+
+
+    }
+}
