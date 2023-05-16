154ef61cb4f87a552b7e3bf5564ca16f199ad885
muzafferugur <av.muzafferugur@gmail.com>
2023-05-16 17:36:03 +0300

    16.05.2023

--- src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
@@ -1,21 +1,36 @@
 package tests.day18_pom;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class C02_ConfigReaderKullanimi {
     @Test
     public void test01() {
          /*
         1.Facebook anasayfaya gidin
-        2.Kullanıcı mail kutusuna rastgele isim yazdırın
-        3.Kullanıcı şifre kutusuna rastgele bir password yazdırın
+        2.Kullanıcı mail kutusuna yanlış kullanıcı ismi yazdırın
+        3.Kullanıcı şifre kutusuna yanlış bir password yazdırın
         4.Login butonuna basın
         5.Giriş yapılamadığını test edin
         6.Sayfayı kapatın
          */
 
+        FacebookPage facebookPage = new FacebookPage();
 
+        Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
 
+        facebookPage.mailKutusu.sendKeys(ConfigReader.getProperty("fbWrongUsername"));
+
+        facebookPage.sifreKutusu.sendKeys(ConfigReader.getProperty("fbWrongPassword"));
+
+        facebookPage.loginTusu.click();
+
+        Assert.assertTrue(facebookPage.girilmediYaziElementi.isDisplayed());
+
+        Driver.closeDriver();
 
     }
 }
