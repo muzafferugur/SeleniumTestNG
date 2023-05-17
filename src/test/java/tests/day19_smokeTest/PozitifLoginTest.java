ee9a648a2877bbb513c6e11a77d0a63c637751e4
muzafferugur <av.muzafferugur@gmail.com>
2023-05-17 21:56:39 +0300

    17.05.2023

--- src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
+++ src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
@@ -5,7 +5,13 @@ import utilities.ConfigReader;
 import utilities.Driver;
 
 public class PozitifLoginTest {
-
+/*
+1.https://www.hotelmycamp.com adresine git
+2.Login butonuna bas
+3.username :manager
+    password :Manager1!
+    değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
+ */
     @Test
     public void test01() {
 
