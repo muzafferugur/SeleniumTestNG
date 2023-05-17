a8efdbf00bf6cea3dcaf33f1957be000a5e45d89
muzafferugur <av.muzafferugur@gmail.com>
2023-05-17 22:36:44 +0300

    17.05.2023

--- src/test/java/tests/day19_smokeTest/NegativeLoginTest.java
+++ src/test/java/tests/day19_smokeTest/NegativeLoginTest.java
@@ -1,5 +1,32 @@
 package tests.day19_smokeTest;
 
-public class NegativeLoginTest {
+import org.testng.annotations.Test;
+import pages.BrcPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
+public class NegativeLoginTest {
+    BrcPage brcPage = new BrcPage();
+
+    /*
+    1.https://www.bluerentalcars.com/ adresine git
+    2.Login butonuna bas
+    3.user email : customer@bluerentalcars.com
+        password : 54321
+        login butonuna tıklayın
+       değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
+     */
+    @Test
+    public void yanlisSifre() {
+
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+
+        brcPage.ilkLoginButonu.click();
+
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
+
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcWrongPassword"));
+
+        brcPage.ikinciLoginButonu.click();
+    }
 }
