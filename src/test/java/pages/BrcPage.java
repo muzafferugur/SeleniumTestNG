1581ebd375363ff48372dbecf5521bad6eee7d38
muzafferugur <av.muzafferugur@gmail.com>
2023-05-17 22:16:52 +0300

    17.05.2023

--- src/test/java/pages/BrcPage.java
+++ src/test/java/pages/BrcPage.java
@@ -1,11 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class BrcPage {
-    BrcPage() {
+    public BrcPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
+    public WebElement ilkLoginButonu;
+
+    @FindBy(xpath = "(//input[@class='form-control is-invalid'])[1]")
+    public WebElement emailTextBox;
+
+    @FindBy(xpath = "(//input[@class='form-control is-invalid'])[2]")
+    public WebElement passwordTextBox;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement ikinciLoginButonu;
+
 
 }
--- src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
+++ src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
@@ -1,20 +1,35 @@
 package tests.day19_smokeTest;
 
 import org.testng.annotations.Test;
+import pages.BrcPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class PozitifLoginTest {
+
+    BrcPage brcPage = new BrcPage();
+
     /*
     1.https://www.bluerentalcars.com/ adresine git
     2.Login butonuna bas
     3.user email : customer@bluerentalcars.com
         password : 12345
         login butonuna tıklayın
-        değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
+       değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
      */
     @Test
     public void positiveLoginTest() {
+
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+
+        brcPage.ilkLoginButonu.click();
+
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
+
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+
+        brcPage.ikinciLoginButonu.click();
+
 
 
     }
