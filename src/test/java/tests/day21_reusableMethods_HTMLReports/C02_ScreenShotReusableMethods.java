d986a5946d32770ec53daa8e225191f634d80ac7
muzafferugur <av.muzafferugur@gmail.com>
2023-05-19 22:29:06 +0300

    19.05.2023

---
+++ src/test/java/tests/day21_reusableMethods_HTMLReports/C02_ScreenShotReusableMethods.java
@@ -0,0 +1,20 @@
+package tests.day21_reusableMethods_HTMLReports;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
+
+public class C02_ScreenShotReusableMethods {
+    @Test
+    public void test01() throws IOException {
+
+        //amazon sayfasına gidip fotoğrafını çekelim
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        ReusableMethods.getScreenshot("amazon");
+        Driver.closeDriver();
+
+    }
+}
