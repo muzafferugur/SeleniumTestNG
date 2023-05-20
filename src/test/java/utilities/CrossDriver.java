7ced72c177692d96ce9b89ad66a8c8c6655b440a
muzafferugur <av.muzafferugur@gmail.com>
2023-05-20 21:33:31 +0300

    20.05.2023

--- src/test/java/utilities/CrossDriver.java
+++ src/test/java/utilities/CrossDriver.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -22,6 +23,11 @@ public class CrossDriver {
 
         if (driver == null) {
             switch (browser) {
+
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
 
                 case "headless-chrome":
                     WebDriverManager.chromedriver().setup();
