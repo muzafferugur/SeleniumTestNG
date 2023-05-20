4bdf6d997b4434bb5840467831b15cbfcb96540e
muzafferugur <av.muzafferugur@gmail.com>
2023-05-20 22:20:04 +0300

    20.05.2023

---
+++ src/test/java/tests/day22_CrossBrowser/C03_DataProvider.java
@@ -0,0 +1,45 @@
+package tests.day22_CrossBrowser;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C03_DataProvider {
+    @Test
+    public void test01() {
+        AmazonPage amazonPage = new AmazonPage();
+        /*
+        amazon sayfasına gidelim
+        nutella için arama yapalım
+        sonuçların nutella içerdiğini test edelim
+         */
+
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        amazonPage.aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        String expectedKelime = "Nutella";
+        String actualSonucYazisi = amazonPage.aramaSonucElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedKelime));
+
+    }
+
+    @Test(dataProvider ="AranacakKelimeler")
+    /*
+    Arayacağımız kelimeleri bir liste gibi tutup bana yollayacak bir veri sağlayıcısı oluşturacağız.
+     */
+    public void test02() {
+        AmazonPage amazonPage = new AmazonPage();
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        /*
+        Nutella, Java, çiğdem ve Netherlands için arama yapalım
+        sonuçların aradığımız kelime içerdiğini test edelim.
+        sayfayı kapatalım
+         */
+
+    }
+}
